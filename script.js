d98393ea6091c8c51c77b6c93712090f203e6cde
KRISHNATHSHINDE <krishnathshinde47@gmail.com>
2023-11-24 21:44:14 +0530

    Added About and Projects Section

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ var typed = new Typed('#element', {
     strings: ['Krishnath', 'a Web Developer.'],
     typeSpeed: 50,
   });
+
+  var typed1 = new Typed('#element1', {
+    strings: ['Project 1','Personal Portfolio Website'],
+    typeSpeed: 50,
+    showCursor : false,
+
+  });
+
+ 
+  var typed1 = new Typed('#element2', {
+    strings: ['Project 2','Text-Emoji Encryption/Decryption'],
+    typeSpeed: 50,
+    showCursor : false,
+    
+  });
+
+ 
